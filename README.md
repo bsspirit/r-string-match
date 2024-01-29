0610c7669b9d4d7f85f51206b54ba232db7190e1
bsspirit <bsspirit@163.com>
2024-01-29 16:41:48 +0800

    初始化算法

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# r-string-match
-用R语言进行字符串匹配
+# 用R语言进行字符串匹配
+
+当前库位置：<https://github.com/bsspirit/r-string-match>
+
++ kmp.r KMP算法
++ baoli.r 暴力匹配算法
++ run.r 运行测试
+
+[用R语言实现字符串快速匹配算法KMP](http://blog.fens.me/r-kmp/)
+
---
+++ run.r
@@ -0,0 +1,37 @@
+setwd("C:/work/R/text/r-string-match")
+rm(list=ls())
+
+source("./baoli.r")
+txt<-"ABCCADZABCCABBC"
+pattern<-"ABCCABB"
+baoli_match(txt,pattern,LOGGER=TRUE)
+baoli_match(txt,pattern,1,LOGGER=TRUE)
+baoli_match(txt,"ABD",LOGGER=TRUE)
+baoli_match(txt,"ABD",1,LOGGER=TRUE)
+
+source("./kmp.r")
+kmp_next("ABC")
+kmp_next("ABCCABB")
+txt<-"ABCCADZABCCABBC"
+pattern<-"ABCCABB"
+kmp_match(txt,pattern,LOGGER=TRUE)
+kmp_match(txt,'ABD',LOGGER=TRUE)
+
+kmp_match(txt,"ABC")
+kmp_match(txt,"ABX",2)
+kmp_match(txt,"ABCCAB")
+w1<-kmp_match(txt,"AB");w1
+substr(txt,w1$start[1],w1$end[1])
+
+txt2<-rep("ABCCADZABCCABBC",100) %>% paste(collapse = ",")
+pattern2<-"ABCCABB"
+kmp_match(txt2,pattern2)
+
+txt3<-readLines("文字.txt") %>% paste(collapse = ",")
+nchar(txt3)
+
+library(stringr)
+pattern3<-"查找文本串中所有匹配模式串的字符"
+kmp = kmp_match(txt3,pattern3,1);kmp
+baoli = baoli_match(txt3,pattern3,1);baoli
+str = str_locate_all(txt3,pattern3);str
