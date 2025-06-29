e2c5be4f65098ebcb6f7431adafb2d62d37322fa
Dan Zhang <bsspirit@163.com>
2025-06-29 11:25:34 +0800

    http://blog.fens.me/r-kdtree/

---
+++ kdtree.r
@@ -0,0 +1,28 @@
+####################
+# http://blog.fens.me/r-kdtree/
+##########################
+
+#install.packages("less")
+
+library(less)
+
+data(iris)
+
+dat<-iris[,1:2]
+head(dat)
+
+#create a kdtree
+kdt <- KDTree$new(dat)
+kdt
+
+# generate some query points that are different to those used to create the kdtree
+q_data <- iris[1:5,1:2] + array(rnorm(10)*0.1, dim=c(5,2))
+q_data
+
+# query the previously generated kdtree
+res<-kdt$query(query_X = q_data, k = 2)
+res
+
+src<-data.frame(col1=dat[res$nn.idx[,1],1],col2=dat[res$nn.idx[,2],2])
+src
+
